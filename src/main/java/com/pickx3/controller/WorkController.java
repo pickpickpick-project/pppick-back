b0e198ed743117303332992f1ea082864193fbd7
KIM-DO-HEE <kjkj014209@gmail.com>
2023-02-05 18:33:44 +0900

    Feat : "상품 등록 컨트롤러 코드 작성"
    - 상품 등록 api 제작
    - Swagger 연결 및 상품 등록 api 테스트 완료
    
    Resolves : #4

---
+++ src/main/java/com/pickx3/controller/WorkController.java
@@ -0,0 +1,47 @@
+package com.pickx3.controller;
+
+import com.pickx3.domain.entity.work_package.Work;
+import com.pickx3.domain.entity.work_package.dto.WorkForm;
+import com.pickx3.service.WorkService;
+import com.pickx3.util.ApiResponseMessage;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@Api(tags = "상품 CRUD 컨트롤러")
+@RestController("/work")
+public class WorkController {
+    @Autowired
+    private WorkService workService;
+
+    /*
+    * 상품 등록
+    * */
+    @ApiOperation(value = "상품 정보 저장", notes = "회원은 상품을 등록할 수 있다")
+    @PostMapping
+    public ResponseEntity<?> createWork(WorkForm workForm){
+        ApiResponseMessage result;
+        HashMap data = new HashMap<>();
+        try{
+            Work work = workService.createWork(workForm);
+            data.put("workNum",work.getWorkNum());
+            data.put("workerNum",work.getWorkNum());
+            data.put("workName",work.getWorkName());
+            data.put("workPrice",work.getWorkPrice());
+            data.put("workDesc",work.getWorkDesc());
+
+            result = new ApiResponseMessage(true, "Success", "200", "",data);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (Exception e) {
+            result = new ApiResponseMessage(false, "Error", "400", e.getMessage());
+            return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+        }
+    }
+}
