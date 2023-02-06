246816b02d975848e8e4e0a3c6ba3610e4936e49
KIM-DO-HEE <kjkj014209@gmail.com>
2023-02-06 14:50:11 +0900

    Feat : "상품 목록 조회 테스트 코드 작성"
    - 상품 목록 조회 테스트 코드 작성
    
    Resolves : #12

--- src/test/java/com/pickx3/domain/repository/WorkRepositoryTest.java
+++ src/test/java/com/pickx3/domain/repository/WorkRepositoryTest.java
@@ -4,9 +4,13 @@ import com.nimbusds.openid.connect.sdk.claims.UserInfo;
 import com.pickx3.domain.entity.user_package.User;
 import com.pickx3.domain.entity.work_package.Work;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,6 +22,12 @@ class WorkRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @BeforeEach
+    public void clear(){
+        userRepository.deleteAll();
+        workRepository.deleteAll();
+    }
 
     @Test
     public void 상품등록(){
@@ -32,6 +42,30 @@ class WorkRepositoryTest {
         Work result = workRepository.save(work);
 
         Assertions.assertEquals(1L, result.getWorkNum());
+    }
+
+    @Test
+    public void 상품목록조회(){
+        User user = 회원생성();
+        List<Work> works = new ArrayList<>();
+
+//      given
+        for(int i = 0; i < 5; i++){
+            Work work =  Work.builder()
+                    .workName("상품"+i)
+                    .workDesc("상품 설명" + i)
+                    .workPrice(25000 + i)
+                    .userInfo(user)
+                    .build();
+
+            workRepository.save(work);
+            works.add(work);
+        }
+
+//      when
+        int size = workRepository.findAll().size();
+
+        Assertions.assertEquals(5, size);
     }
 
     public User 회원생성(){
