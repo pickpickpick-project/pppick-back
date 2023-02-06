c43f79d6c3879c03db1208e2f7962545da5a8ce4
KIM-DO-HEE <kjkj014209@gmail.com>
2023-02-06 19:25:49 +0900

    Feat : "회원별 상품 목록 조회 컨트롤러 코드 작성"
    - 회원별 상품 목록 조회 api 제작
    - Swagger 연결 및 상품 목록 조회 api 테스트 완료
    
    Resolves : #12

--- src/main/java/com/pickx3/controller/WorkController.java
+++ src/main/java/com/pickx3/controller/WorkController.java
@@ -10,10 +10,13 @@ import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 
 @Api(tags = "상품 CRUD 컨트롤러")
 @RestController("/work")
@@ -36,6 +39,26 @@ public class WorkController {
             data.put("workName",work.getWorkName());
             data.put("workPrice",work.getWorkPrice());
             data.put("workDesc",work.getWorkDesc());
+
+            result = new ApiResponseMessage(true, "Success", "200", "",data);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (Exception e) {
+            result = new ApiResponseMessage(false, "Error", "400", e.getMessage());
+            return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    /*
+     * 회원별 상품 목록 조회
+     * */
+    @ApiOperation(value = "회원별 상품 목록 조회", notes = "회원은 자신이 등록한 상품 목록을 조회 할 수 있다")
+    @GetMapping
+    public ResponseEntity<?> getWorks(@RequestParam(value = "workerNum") Long workerNum){
+        ApiResponseMessage result;
+        HashMap data = new HashMap<>();
+        try{
+            List<Work> works = workService.getWorks(workerNum);
+            data.put("works",works);
 
             result = new ApiResponseMessage(true, "Success", "200", "",data);
             return new ResponseEntity<>(result, HttpStatus.OK);
