afea264a06b2c949b4c0b847fb9e26eaaa36c97a
KIM-DO-HEE <kjkj014209@gmail.com>
2023-02-11 19:07:51 +0900

    refactor: 결제 엔티티 필드 및 빌더 추가
    - 주문 아이디, pg사, 결제상태 필드 추가
    - user 필드명 변경
    - 결제날짜 Date->LocalDate로 변경

--- src/main/java/com/pickx3/domain/entity/work_package/Pay.java
+++ src/main/java/com/pickx3/domain/entity/work_package/Pay.java
@@ -1,17 +1,25 @@
 package com.pickx3.domain.entity.work_package;
 
 import com.pickx3.domain.entity.user_package.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.Date;
+import java.time.LocalDate;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class Pay{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long payNum;
 
-    private Date payDate;
+    private String merchant_uid;
+
+    private LocalDate payDate;
 
     private int payPrice;
 
@@ -19,9 +27,13 @@ public class Pay{
 
     private int payCount;
 
+    private int payStatus;
+
+    private String pg;
+
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
     @JoinColumn(name="payerNum")
-    private User userInfo;
+    private User user;
 
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
     @JoinColumn(name="workNum")
