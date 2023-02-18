738ded05b5f834129b5c5cf5ef9a94cd78a7dbf5
iiolo <alohalo98@naver.com>
2023-02-18 14:24:40 +0900

    feat : user 정보 수정 기능 추가 - userNick, userIntro, userPhone, userImg 변경 가능 - userImg의 경우 이미 설정된 userImg 존재 시 해당 경로 및 사진 삭제 후 새로운 이미지가 폴더에 저장되고 경로가 디비에 저장됨

--- src/main/java/com/pickx3/service/UserService.java
+++ src/main/java/com/pickx3/service/UserService.java
@@ -6,6 +6,7 @@ import com.pickx3.domain.entity.portfolio_package.Favorites;
 import com.pickx3.domain.entity.portfolio_package.Portfolio;
 import com.pickx3.domain.entity.post_package.Post;
 import com.pickx3.domain.entity.user_package.User;
+import com.pickx3.domain.entity.user_package.dto.UserUpdateRequestDto;
 import com.pickx3.domain.repository.FavoriteRepository;
 import com.pickx3.domain.repository.PortfolioRepository;
 import com.pickx3.domain.repository.UserRepository;
@@ -15,8 +16,16 @@ import com.pickx3.service.post_package.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -42,75 +51,82 @@ public class UserService {
     }
 
 
-    //    @Transactional
-//    public Long update(UserUpdateRequestDto requestDto) throws IOException {
-//        User user = userRepository.findById(requestDto.getUserNum())
-//                .orElseThrow(() -> new
-//                        IllegalArgumentException("해당 유저가 존재하지 않습니다."));
-//        user.setNickName(requestDto.getUserNick());
-//        user.setPhone(requestDto.getUserPhone());
-//        user.setIntro(requestDto.getUserIntro());
-//
-//        File file = requestDto.getUserImg();
-//
-//
-//
-//        if(file!=null){
-//            // 파일명을 업로드 한 날짜로 변환하여 저장
-//            LocalDateTime now = LocalDateTime.now();
-//            DateTimeFormatter dateTimeFormatter =
-//                    DateTimeFormatter.ofPattern("yyyyMMdd");
-//            String current_date = now.format(dateTimeFormatter);
-//
-//            // 프로젝트 디렉터리 내의 저장을 위한 절대 경로 설정
-//            // 경로 구분자 File.separator 사용
-//            String absolutePath = new File("").getAbsolutePath() + File.separator + File.separator;
-//
-//
-//            // 파일을 저장할 세부 경로 지정
-//            String path = "images" + File.separator + "user" + File.separator + current_date+File.separator;
-//
-//            File newFile = new File(path);
-//            // 디렉터리가 존재하지 않을 경우
-//            if(!newFile.exists()) {
-//                boolean wasSuccessful = newFile.mkdirs();
-//
-//                // 디렉터리 생성에 실패했을 경우
-//                if(!wasSuccessful)
-//                    System.out.println("file: was not successful");
-//            }
-//
-//            //확장자 처리
-//            String originalFileExtension = null;
-//            String fileName = file.getName();
-//            String contentType = fileName.substring(fileName.lastIndexOf(".") + 1);
-//
-//            // 확장자명이 존재하지 않을 경우 처리 x
-//            if(!ObjectUtils.isEmpty(contentType)) {
-//                if(contentType.contains("image/jpeg"))
-//                    originalFileExtension = ".jpg";
-//                else if(contentType.contains("image/png"))
-//                    originalFileExtension = ".png";
-//            }
-//
-//            // 파일명 중복 피하고자 나노초까지 얻어와 지정
-//            String new_file_name = System.nanoTime() + originalFileExtension;
-//            newFile = new File(absolutePath + path + File.separator + new_file_name);
-//
-//            // 파일 권한 설정(쓰기, 읽기)
-//            newFile.setWritable(true);
-//            newFile.setReadable(true);
-//
-//            user.setImageUrl(newFile.getPath());
-//
-//        }
-//
-//        userRepository.save(user);
-//
-//
-//        return requestDto.getUserNum();
-//
-//    }
+    @Transactional
+    public User update(Long userNum,UserUpdateRequestDto requestDto) throws IOException {
+        String userNickname = requestDto.getUserNick();
+        String userIntro = requestDto.getUserIntro();
+        String userPhone = requestDto.getUserPhone();
+
+
+        User user = userRepository.findById(userNum).get();
+
+        user.updateUser(userNickname, userIntro,userPhone);
+
+        MultipartFile file = requestDto.getUserImg();
+
+
+
+        if(file!=null){
+            if(user.getImageUrl()!=null){
+                if(user.getImageUrl().startsWith("images")){
+                    String absolutePath = new File("").getAbsolutePath() + File.separator;
+                    Path filePath = Paths.get(absolutePath+user.getImageUrl());
+                    Files.delete(filePath);
+                }
+                user.setImageUrl(null);
+            }
+
+            // 파일명을 업로드 한 날짜로 변환하여 저장
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter dateTimeFormatter =
+                    DateTimeFormatter.ofPattern("yyyyMMdd");
+            String current_date = now.format(dateTimeFormatter);
+
+            // 프로젝트 디렉터리 내의 저장을 위한 절대 경로 설정
+            // 경로 구분자 File.separator 사용
+            String absolutePath = new File("").getAbsolutePath() + File.separator + File.separator;
+
+
+            // 파일을 저장할 세부 경로 지정
+            String path = "images" + File.separator + "user" + File.separator + current_date+File.separator;
+
+            File newFile = new File(path);
+            // 디렉터리가 존재하지 않을 경우
+            if(!newFile.exists()) {
+                boolean wasSuccessful = newFile.mkdirs();
+
+                // 디렉터리 생성에 실패했을 경우
+                if(!wasSuccessful)
+                    System.out.println("file: was not successful");
+            }
+
+            //확장자 처리
+            String originalFileExtension = null;
+            String contentType = file.getContentType();
+
+            // 확장자명이 존재하지 않을 경우 처리 x
+            if(!ObjectUtils.isEmpty(contentType)) {
+                if(contentType.contains("image/jpeg"))
+                    originalFileExtension = ".jpg";
+                else if(contentType.contains("image/png"))
+                    originalFileExtension = ".png";
+            }
+
+            // 파일명 중복 피하고자 나노초까지 얻어와 지정
+            String new_file_name = System.nanoTime() + originalFileExtension;
+            newFile = new File(absolutePath + path + File.separator + new_file_name);
+            file.transferTo(newFile);
+            // 파일 권한 설정(쓰기, 읽기)
+            newFile.setWritable(true);
+            newFile.setReadable(true);
+
+            user.setImageUrl(path+new_file_name);
+
+        }
+
+        return user;
+
+    }
     @Transactional
     public void delete(Long userNum) throws IOException {
         User user = userRepository.findById(userNum)
