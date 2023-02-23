74f3cf64795183dd920063a69155e9b545f878b8
KIM-DO-HEE <devkdhee@gmail.com>
2023-02-23 16:54:37 +0900

    add : Enum타입 클래스 및 주문상세DTO 추가

---
+++ src/main/java/com/pickx3/domain/entity/work_package/dto/orders/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.pickx3.domain.entity.work_package.dto.orders;
+
+public enum OrderStatus {
+    CONFIRM, COMPLETE, CANCEL
+}
---
+++ src/main/java/com/pickx3/domain/entity/work_package/dto/pay/PaymentStatus.java
@@ -0,0 +1,5 @@
+package com.pickx3.domain.entity.work_package.dto.pay;
+
+public enum PaymentStatus {
+    COMPLETE, CANCEL
+}
